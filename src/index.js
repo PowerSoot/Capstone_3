4a7d6e0fbc0eac2708d9503835bb8faf41e40653
powersoot <powersoot@outlook.com>
2025-02-15 05:52:23 -1200

    created and used branch addBrowserRouter to setup BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 const root = createRoot(window.bodyTag);
-root.render(<main></main>);
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route></Route>
+    <Route></Route>
+    <Route></Route>
+  </Routes>
+  </BrowserRouter>
+);
